35be0fd064630619e5a501d34805a2db4c427d00
uechi-y <uechi.y@gmail.com>
2019-09-04 08:22:32 +0000

    fix pylint

--- 002/002.py
+++ 002/002.py
@@ -1,7 +1,8 @@
 def fib(n):
     f = [1, 2]
-    while (f[-1]+f[-2] < n):
+    while f[-1]+f[-2] < n:
         f.append(f[-1] + f[-2])
     return f
 
-print(sum([i for i in fib(4000000) if i % 2==0]))
+
+print(sum([i for i in fib(4000000) if i % 2 == 0]))
